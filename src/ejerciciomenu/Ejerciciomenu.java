dccfe3518420b6c3539ca4ee117e09f9bb97754c
USUARIO <USUARIO@PC18>
2019-10-10 10:26:22 -0500

    SE INCLUYEN LOS ATRIBUTOS

--- src/ejerciciomenu/Ejerciciomenu.java
+++ src/ejerciciomenu/Ejerciciomenu.java
@@ -24,74 +24,47 @@ public class Ejerciciomenu {
           try{  
             
    opcion = Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor de la operacion que quiere realizar"
-                    + "\n 1. Mostrar operaciones aritmeticas"
-                    + "\n 2. Mostrar Departamentos"
-                    + "\n 3. Mostrar Ciudades"
-                    + "\n 4. Salir"));
+                    + "\n 1. QUE ES UN COMMIT?"
+                    + "\n 2. QUE ES UN PUSH?"
+                    + "\n 3. QUE ES UN PULL?"
+                    + "\n 4. QUE ES UNA CLASE"
+                    + "\n 5. QUE ES UN OBJETO"
+                    + "\n 6. NOMBRE DEL ESTUDIANTE"
+                    + "\n 7. FECHA"
+                    + "\n 8. SALIR"));
        
           }catch(Exception ex){
-              JOptionPane.showMessageDialog(null,"Debe ingresar solo valores numericos");
+              JOptionPane.showMessageDialog(null,"Debe ingresar solo  valores numericos");
           }
             switch(opcion){
                 case 1:
-                    do{
-                    op=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor de la operacion que quiere realizar"
-                    + "\n 1. Suma"
-                    + "\n 2. Resta"
-                    + "\n 3. Multiplicacion"
-                    + "\n 4. Division"
-                    + "\n 5. Atras"));
-                   switch(op){
-                       case 1:
-                           numero1=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor del primer numero"));
-                           numero2=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor del segundo numero"));
-                           JOptionPane.showMessageDialog(null, "El resultado es igual a ="+(numero1 + numero2));
-                           break;
-                           
-                       case 2:    
-                           numero1=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor del primer numero"));
-                           numero2=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor del segundo numero"));
-                           JOptionPane.showMessageDialog(null, "El resultado es igual a ="+(numero1 - numero2));
-                           break;
-                           
-                       case 3:    
-                           numero1=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor del primer numero"));
-                           numero2=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor del segundo numero"));
-                           JOptionPane.showMessageDialog(null, "El resultado es igual a ="+(numero1 * numero2));
-                           break;
-                           
-                       case 4:    
-                           numero1=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor del primer numero"));
-                           numero2=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor del segundo numero"));
-                           JOptionPane.showMessageDialog(null, "El resultado es igual a ="+(numero1 / numero2));
-                           break;
-                           
-                       case 5:
-                           JOptionPane.showMessageDialog(null,"Regresaste al menu principal");
-                           break;
-                           
-                        default:
-                            JOptionPane.showMessageDialog(null,"Su opcion no es valida");
-                        break;
-                   }
-                   }while (op !=5);
+                    JOptionPane.showMessageDialog(null, " Hacer un commit se refiere,\n en el contexto de la ciencia de la computación y la gestión de datos,\n a la idea de confirmar un conjunto de cambios provisionales de forma permanente. \n Un uso popular es al final de una transacción de base de datos.");
                    break;
                    
                 case 2:
-                    JOptionPane.showMessageDialog(null, "Mostrar departamentos");
+                    JOptionPane.showMessageDialog(null, "");
                     break;
                 case 3:
                     
-                    JOptionPane.showMessageDialog(null, "Mostrar ciudades");
+                    JOptionPane.showMessageDialog(null, "");
                     break;
                 case 4:
-                    JOptionPane.showMessageDialog(null, "Seleccionó Salir");
+                    JOptionPane.showMessageDialog(null, "");
                     break;
+                case 5:
+                    JOptionPane.showMessageDialog(null, "");
+                    break;  
+                case 6:
+                    JOptionPane.showMessageDialog(null, "INGRESE SU NOMBRE");
+                    break;    
+                case 7:
+                    JOptionPane.showMessageDialog(null, "");
+                    break;    
                 default:
                     JOptionPane.showMessageDialog(null, "Ingresó una opción no valida");
                    break;
             }
-            }while(opcion != 4);
+            }while(opcion != 8);
     }
     
 }

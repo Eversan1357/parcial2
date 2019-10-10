89b6366764b34d0cb94b05b86a16dfb8a035a410
USUARIO <USUARIO@PC18>
2019-10-10 10:36:52 -0500

    SE  MAS ATRIBUTOS

--- src/ejerciciomenu/segundo.java
+++ src/ejerciciomenu/segundo.java
@@ -1,39 +1,55 @@
 package ejerciciomenu;
 
+import javax.swing.JOptionPane;
+
 
 public class segundo {
-    private int id;
+   private int id;
     private String fecha;
     private String nombre;
 
-    public int getId() {
+    public int getid() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setid(int id) {
         this.id = id;
     }
 
-    public String getFecha() {
+    public String getfecha() {
         return fecha;
     }
 
-    public void setFecha(String fecha) {
+    public void setfecha(String fecha) {
         this.fecha = fecha;
     }
 
-    public String getNombre() {
+    public String getnombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
+    public void setnombre(String nombre) {
         this.nombre = nombre;
     }
     
+    public void commit(){
+        JOptionPane.showMessageDialog(null, "un Commit identifica los cambios hechos en dicho ambiente de trabajo.");
+    }
     
+    public void push(){
+        JOptionPane.showMessageDialog(null, " un git push sube los cambios al repositorio central, comúnmente llamado origin, para que otros desarrolladores puedan bajarse tus cambios");
+    }
     
-   
+    public void pull(){
+        JOptionPane.showMessageDialog(null, "");
+    }
     
+    public void clase(){
+        JOptionPane.showMessageDialog(null, "");
+    }
     
-    
+    public void objeto(){
+        JOptionPane.showMessageDialog(null, "");
+    }
 }
+
